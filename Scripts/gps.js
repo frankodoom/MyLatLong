d6a5e548e47192ce975182c8c0945d213ccca4fb
Frank Arkhurst Odoom <frank@accedegh.com>
2016-09-01 14:36:04 +0000

    Add files via upload

---
+++ Scripts/gps.js
@@ -0,0 +1,20 @@
+﻿
+function getLocation() {
+    // check for Geolocation support
+    if (navigator.geolocation) {
+        //console.log('Geolocation is supported!');
+        alert('Geolocation is supported')
+        var startPos;
+        var geoSuccess = function (position) {
+            startPos = position;
+            document.getElementById('startLat').value = startPos.coords.latitude;
+            document.getElementById('startLon').value = startPos.coords.longitude;
+        };
+        navigator.geolocation.getCurrentPosition(geoSuccess);
+    }
+    else {
+        //console.log('Geolocation is not supported for this Browser/OS version yet.');
+        alert('Geolocation is not supported for this Browser/OS version yet.')      
+    }
+    
+};
